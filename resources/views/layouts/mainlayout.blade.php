809b8480b5a6fb84056cf8ad384dced4259637cd
ReganRK <reganreinal@gmail.com>
2023-10-20 15:32:20 +0700

    Migration
    Adding migration to my laravel

--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -30,7 +30,9 @@
         </div>
     </nav>
 
-    @yield('content')
+    <div class="container-fluid">
+        @yield('content')
+    </div>
 
     <!-- script bootsrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
