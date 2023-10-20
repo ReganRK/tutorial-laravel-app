98fa1c4c875b589b9a9aeb226aa0ed80e52be827
ReganRK <reganreinal@gmail.com>
2023-10-20 13:37:12 +0700

    ParentChildLayout
    Add layout for view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,18 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel-tutorial-Home</title>
+    <title>Laravel-tutorial-Home</title> -->
     <!-- bootsrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Tutorial Laravel</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Product</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-    </nav>
+    <div class="container-fluid"> -->
+    @extends('layouts.mainlayout')
+    @section('title', 'Home')
 
-    <div class="container-fluid">
+    @section('content')
         <h1>
             Ini Home
         </h1>
@@ -39,10 +21,58 @@
             Selamat datang, {{$name}} sebagai {{$role}}
         </h2>
 
-        <button type="button" class="btn btn-primary">Primary</button>
-    </div>
+        <!-- @if ($role == 'Admin')
+            ini admin
+        @else
+            ini bukan admin
+        @endif -->
+
+        <!-- @switch($role)
+            @case($role == 'Admin')
+                ini admin
+                @break
+            
+            @case($role == 'Staff')
+                ini staff
+                @break
+            
+            @case($role == 'Public')
+                ini public
+                @break
+
+        @endswitch -->
+
+        @for ($i = 0; $i < 10; $i++)
+            The current value is {{ $i }}
+            <br>
+        @endfor
+
+        <p>
+            We have:
+        </p>
+
+        <table class="table">
+            <tr>
+                <th>No</th>
+                <th>Name</th>
+            </tr>
+            @foreach ($products as $product)
+                <tr>
+                    <td>{{$loop->iteration}}</td>
+                    <td>{{$product}}</td>
+                </tr>
+            @endforeach    
+        </table>
+    @endsection
+    
+
+        <!-- @foreach ($products as $product)
+            <p>- {{$product}}</p>
+        @endforeach -->
+
+    <!-- </div> -->
 
     <!-- script bootsrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
-</html>
+</html> -->
